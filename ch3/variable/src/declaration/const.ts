4ba230f52e7c2b9e42f203b7a4300c4548157d5c
jaehun.lee <jaehun2841@gmail.com>
2019-10-27 17:32:18 +0900

    add: added chapter3 codes (typescript types)

---
+++ ch3/variable/src/declaration/const.ts
@@ -0,0 +1,18 @@
+const birthlMonth = 9;
+if(true) {
+    const birthlMonth = 12;
+}
+
+console.log(birthlMonth);
+
+const profile = {
+    name: "happy",
+    month: birthlMonth
+};
+
+//const profile = "happy";
+profile.name = "happy2";
+profile.month--;
+
+console.log(profile);
+ 
